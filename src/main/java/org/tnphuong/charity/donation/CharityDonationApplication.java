453726602b85972a2b30fcce53b1349c90503b8e
Scove <104053980+Scove-Metalreal@users.noreply.github.com>
2026-02-14 16:53:55 +0700

    Initial project import with Maven wrapper
    Add initial project skeleton: Maven wrapper (mvnw, mvnw.cmd, .mvn/wrapper/maven-wrapper.properties), pom.xml, .gitignore and .gitattributes. Include basic Spring Boot app (CharityDonationApplication.java), application.properties and a test class. Add numerous documentation and design assets under Documents (HLD, SRS, use cases, diagrams, drawio and MDJ files).

---
+++ src/main/java/org/tnphuong/charity/donation/CharityDonationApplication.java
@@ -0,0 +1,13 @@
+package org.tnphuong.charity.donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityDonationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityDonationApplication.class, args);
+    }
+
+}
